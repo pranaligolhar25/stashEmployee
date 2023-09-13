fc8497f3078a2de1bce2fc4ade331a58fa678a3b
poojapisal <shindepooja2496@gmail.com>
2023-09-13 22:11:17 +0530

    Post & Get API Created in homecontroller

--- StachCrudHW/src/main/java/com/main/Controller/EmployeeController.java
+++ StachCrudHW/src/main/java/com/main/Controller/EmployeeController.java
@@ -1,8 +1,16 @@
 package com.main.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.main.Model.Employee;
 import com.main.servises.EmployeeServise;
 
 @RestController
@@ -11,5 +19,20 @@ public class EmployeeController
 	
 	@Autowired
 	EmployeeServise employeeServise;
+	@PostMapping("/saveEmployee")
+	public ResponseEntity<Employee> saveData(@RequestBody Employee emp)
+	{
+		Employee empDb=employeeServise.saveData(emp);
+		return new ResponseEntity<Employee>(empDb, HttpStatus.CREATED);
+		
+		
+	}
+	@GetMapping("/GetAllData")
+	public ResponseEntity<List<Employee>> getAllData()
+	{
+		List<Employee> empList=employeeServise.getAllData();
+		return new ResponseEntity<List<Employee>>(empList, HttpStatus.OK);
+
+	}
 
 }
--- StachCrudHW/src/main/java/com/main/servises/EmployeeServise.java
+++ StachCrudHW/src/main/java/com/main/servises/EmployeeServise.java
@@ -1,5 +1,15 @@
 package com.main.servises;
 
+import java.util.List;
+
+import com.main.Model.Employee;
+
 public interface EmployeeServise {
+
+	Employee saveData(Employee emp);
+
+	List<Employee> getAllData();
+
+	Employee getDataById(int empId);
 
 }
